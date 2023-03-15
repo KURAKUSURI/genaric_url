7c776355f6acfc9d64ebefb7312083df5f3c2f65
KURAKUSURI <kurakusurendra267@gmail.com>
2023-03-16 00:03:03 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def sudha(request):
+    return render(request,'sudha.html')
+
+def nanda(request):
+    return render(request,'nanda.html')
